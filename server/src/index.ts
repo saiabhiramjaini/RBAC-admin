1883eed1f5bda809b28551e6c0e48f74b75c1075
saiabhiramjaini <abhiramjaini28@gmail.com>
2024-11-26 21:56:50 +0530

    v0 done

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,8 +11,8 @@ const app= express();
 
 app.use(cors({
   origin: ['http://localhost:5173', 
-    "https://rbac.abhiramverse.tech/",
-    "https://rbac-admin-sage.vercel.app/"],
+    "https://rbac.abhiramverse.tech",
+    "https://rbac-admin-sage.vercel.app"],
   credentials: true,
 }));
 app.use(express.json());
